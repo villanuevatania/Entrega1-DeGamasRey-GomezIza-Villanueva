d87e364ed5d2d5a25b9d278ab25f95c78ec13ea1
Tania Villanueva <taniavillanueva@hotmail.com.ar>
2022-07-11 18:30:55 -0300

    Botón de búsqueda

--- bandas/forms.py
+++ bandas/forms.py
@@ -4,3 +4,6 @@ class FormBanda(forms.Form):
     nombre = forms.CharField(max_length=30)
     genero = forms.CharField(max_length=30)
     anios_activa = forms.IntegerField(required=False)
+
+class BusquedaBanda (forms.Form):
+    nombre = forms.CharField(max_length=30)
--- bandas/views.py
+++ bandas/views.py
@@ -1,8 +1,9 @@
+from django import forms
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-from .forms import FormBanda
+from .forms import BusquedaBanda, FormBanda
 from .models import Banda
 
 # Create your views here.
@@ -38,10 +39,17 @@ def crear_banda(request):
   return render(request, 'crear_banda.html', {'form': form_banda})
 
 def listado_bandas(request):
-  listado_bandas = Banda.objects.all()
-  return render(request, 'listado_bandas.html', {'listado_bandas': listado_bandas})
-      
   
+  nombre_de_busqueda = request.GET.get('banda')
+  
+  if nombre_de_busqueda:
+    listado_bandas = Banda.objects.filter(banda__icontains = nombre_de_busqueda)
+  else:
+    listado_bandas = Banda.objects.all()
+    form = BusquedaBanda()
+  
+  return render(request, 'listado_bandas.html', {'listado_bandas': listado_bandas, 'form':form})
+
 
 def about(request):
     return HttpResponse ('<h1> Comunidad DO RE MI fue creado con la finalidad de que puedas cargar una reseña sobre tu/s banda/s, difundir tus shows, conocer nuevas bandas e intercambiar opiniones con otros músicos y/o melómanos.<h1>'
