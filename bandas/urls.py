eefccd820aa0a7173eae8b865584e6e51284cb63
Tania Villanueva <taniavillanueva@hotmail.com.ar>
2022-07-12 17:20:19 -0300

    Correcciones menoresÂ

--- bandas/urls.py
+++ bandas/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import home, crear_banda, about, listado_bandas
 
 urlpatterns = [
-    path('mi-template/', crear_banda, name='crear_banda'),
-    path('mi-template/', listado_bandas, name='listado_bandas'),
+    path('crear-banda/', crear_banda, name='crear_banda'),
+    path('listado-bandas/', listado_bandas, name='listado_bandas'),
     path('home', about, name='about'),
     path('', home, name='home'),
 ]
--- bandas/views.py
+++ bandas/views.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import loader
+from django.shortcuts import redirect, render
 
 from .forms import BusquedaBanda, FormBanda
 from .models import Banda
