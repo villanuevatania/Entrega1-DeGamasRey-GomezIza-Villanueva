11908a92b4182862f86233b3b47b07efc77f526b
Tania Villanueva <taniavillanueva@hotmail.com.ar>
2022-08-02 00:31:00 -0300

    Creación de superusuario

--- bandas/urls.py
+++ bandas/urls.py
@@ -3,10 +3,10 @@ from bandas import views
 from .views import home, crear_banda, about, listado_bandas, buscar, eliminar_bandas, mostrar_bandas, editar_bandas
 
 urlpatterns = [
+    path('', home, name='home'),
     path('crear-banda/', crear_banda, name='crear_banda'),
     path('listado-bandas/', listado_bandas, name='listado_bandas'),
     path('about/', about, name='about'),
-    path('', home, name='home'),
     path('buscar/', buscar, name='buscador'),
     path('editar-bandas/<int:id>/', editar_bandas, name='editar_bandas'),
     path('eliminar-bandas/<int:id>/', eliminar_bandas, name='eliminar_bandas'),
