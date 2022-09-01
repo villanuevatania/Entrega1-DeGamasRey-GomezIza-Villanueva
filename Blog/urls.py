5cfab93173893ed54d451b0468bc444d502f1e22
Tania Villanueva <taniavillanueva@hotmail.com.ar>
2022-09-01 18:56:40 -0300

    Corrección en perfilÂ

--- Blog/urls.py
+++ Blog/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('', include('bandas.urls')),
     path('bandas/', include('bandas.urls')),
     path('accounts/', include('accounts.urls')),
-    path('posteo/', include('posteo.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- posteo/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def posteo (request):
-    # listado_bandas = Banda.objects.all()
-    
-    # form = BusquedaBanda()
-    return 'Soy un posteo'
